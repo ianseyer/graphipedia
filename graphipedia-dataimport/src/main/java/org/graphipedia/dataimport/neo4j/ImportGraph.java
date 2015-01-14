90bcb2a11e8f4f9798fe076f44c0417162cff612
Sebastian Straub <sstraub@avantgarde-labs.de>
2015-01-14 13:06:41 +0100

    automatically calculates and sets neo4j memory requirements

--- graphipedia-dataimport/src/main/java/org/graphipedia/dataimport/neo4j/ImportGraph.java
+++ graphipedia-dataimport/src/main/java/org/graphipedia/dataimport/neo4j/ImportGraph.java
@@ -32,8 +32,8 @@ public class ImportGraph {
 	private final BatchInserter inserter;
 	private final Map<String, Long> inMemoryIndex;
 
-	public ImportGraph(String dataDir, boolean append) {
-		inserter = BatchInserters.inserter(dataDir);
+	public ImportGraph(String dataDir, boolean append, int mNodes, int mEdges) {
+		inserter = BatchInserters.inserter(dataDir, generateConfig(mNodes, mEdges));
 		if( !append ) {
 			inserter.createDeferredSchemaIndex(WikiLabel.Page).on(WikiNodeProperty.title.name()).create();
 			inserter.createDeferredSchemaIndex(WikiLabel.Page).on(WikiNodeProperty.lang.name()).create();
@@ -45,16 +45,46 @@ public class ImportGraph {
 		inMemoryIndex = new HashMap<String, Long>();
 	}
 
+	/**
+	 * Generates a configuration suitable for batch imports. Reserves enough space to fit nodes and
+	 * relationships into memory. Estimates based on
+	 * http://neo4j.com/docs/stable/configuration-io-examples.html#configuration-batchinsert
+	 * @param mNodes expected number of nodes to import (in millions)
+	 * @param mEdges expected number of edges to import (in millions)
+	 * @return a neo4j configuration that provides enough memory for an import of the specified size
+	 */
+	protected Map<String, String> generateConfig(int mNodes, int mEdges) {
+	  Map<String, String> config = new HashMap<>();
+	  int required = (mNodes * 9) + (mEdges * 33);
+	  String nodes = (mNodes * 9) + "M";
+	  String edges = (mEdges * 33) + "M";
+    config.put( "neostore.nodestore.db.mapped_memory", nodes );
+    config.put( "neostore.relationshipstore.db.mapped_memory", edges );
+    config.put( "eostore.propertystore.db.mapped_memory", "50M" );
+    config.put( "neostore.propertystore.db.strings.mapped_memory", "100M" );
+    config.put( "neostore.propertystore.db.arrays.mapped_memory", "0M" );
+
+    System.out.printf("To keep everything in memory, neo4j will require a total of %s for nodes "
+        + "and %s for edges.\n", nodes, edges);
+    long xmx = Runtime.getRuntime().maxMemory() / 1048576;
+    System.out.printf("The JVM has %sM available, which is%s enough to keep everything in memory.\n",
+        xmx, xmx > required ? "" : " not");
+
+	  return config;
+	}
+
 	public static void main(String[] args) throws Exception {
 		if (args.length < 4) {
-			System.out.println("USAGE: ImportGraph <input-file> <data-dir> <lang-code> <append>");
+			System.out.println("USAGE: ImportGraph <input-file> <data-dir> <lang-code> (<append> <million-nodes> <milltion-edges>)");
 			System.exit(255);
 		}
 		String inputFile = args[0];
 		String dataDir = args[1];
 		String langCode = args[2];
-		boolean append = Boolean.parseBoolean(args[3]);
-		ImportGraph importer = new ImportGraph(dataDir, append);
+		boolean append = args.length > 3 ? Boolean.parseBoolean(args[3]) : false;
+		int mNodes = args.length > 4 ? Integer.parseInt(args[4]) : 10;
+		int mEdges = args.length > 5 ? Integer.parseInt(args[5]) : 100;
+		ImportGraph importer = new ImportGraph(dataDir, append, mNodes, mEdges);
 		importer.createNodes(inputFile, langCode);
 		importer.createRelationships(inputFile);
 		importer.finish();
@@ -66,7 +96,7 @@ public class ImportGraph {
 		long startTime = System.currentTimeMillis();
 		nodeCreator.parse(fileName);
 		long elapsedSeconds = (System.currentTimeMillis() - startTime) / 1000;
-		System.out.printf("\n%d pages and %d categories imported in %d seconds.\n", nodeCreator.getNumberOfPages(), 
+		System.out.printf("\n%d pages and %d categories imported in %d seconds.\n", nodeCreator.getNumberOfPages(),
 				nodeCreator.getNumberOfCategories(), elapsedSeconds);
 	}
 
